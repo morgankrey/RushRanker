7ae35c4b4575f58bc78adb767a4fc0123df4562d
Morgan Krey <morgankrey@gmail.com>
2013-12-27 12:34:57 -0500

    Added about page

--- rushranker_project/rushranking/urls.py
+++ rushranker_project/rushranking/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns,url
 from rushranking import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = patterns('',
+   url(r'^$', views.index, name='index'),
+   url(r'^about/$',views.about,name='about'))
--- rushranker_project/rushranking/views.py
+++ rushranker_project/rushranking/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-   return HttpResponse("Working")
+   return HttpResponse("Link to the about page: <a href='/rushranking/about/'>About</a>")
+
+def about(request):
+   return HttpResponse("This is the about page. Back to the index page: <a href='/rushranking/'>Index</a>")
